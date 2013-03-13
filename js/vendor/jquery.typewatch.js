0487ce5d2a3bb0b89eceabc8464a2c954961a037
Miska Närhi <miska@pulse247.info>
2013-03-13 07:34:30 +0200

    New typewatch library to fix livesearch issues

--- js/vendor/jquery.typewatch.js
+++ js/vendor/jquery.typewatch.js
@@ -1,48 +1,55 @@
 /*
- *	TypeWatch 2.0 - Original by Denny Ferrassoli / Refactored by Charles Christolini
- *
- *	Examples/Docs: www.dennydotnet.com
- *
- *	Copyright(c) 2007 Denny Ferrassoli - DennyDotNet.com
- *	Coprright(c) 2008 Charles Christolini - BinaryPie.com
- *
- *	Dual licensed under the MIT and GPL licenses:
- *	http://www.opensource.org/licenses/mit-license.php
- *	http://www.gnu.org/licenses/gpl.html
+*	TypeWatch 2.1
+*
+*	Examples/Docs: github.com/dennyferra/TypeWatch
+*	
+*  Copyright(c) 2013 
+*	Denny Ferrassoli - dennyferra.com
+*   Charles Christolini
+*  
+*  Dual licensed under the MIT and GPL licenses:
+*  http://www.opensource.org/licenses/mit-license.php
+*  http://www.gnu.org/licenses/gpl.html
 */
 
 (function(jQuery) {
-	jQuery.fn.typeWatch = function(o){
+	jQuery.fn.typeWatch = function(o) {
+		// The default input types that are supported
+		var _supportedInputTypes =
+			['TEXT', 'TEXTAREA', 'PASSWORD', 'TEL', 'SEARCH', 'URL', 'EMAIL', 'DATETIME', 'DATE', 'MONTH', 'WEEK', 'TIME', 'DATETIME-LOCAL', 'NUMBER', 'RANGE'];
+
 		// Options
 		var options = jQuery.extend({
-			wait : 750,
-			callback : function() { },
-			highlight : true,
-			captureLength : 2
+			wait: 750,
+			callback: function() { },
+			highlight: true,
+			captureLength: 2,
+			inputTypes: _supportedInputTypes
 		}, o);
 
 		function checkElement(timer, override) {
-			var elTxt = jQuery(timer.el).val();
+			var value = jQuery(timer.el).val();
 
-			// Fire if text > options.captureLength AND text != saved txt OR if override AND text > options.captureLength
-			if ((elTxt.length > options.captureLength && elTxt.toUpperCase() != timer.text)
-			|| (override && elTxt.length > options.captureLength)) {
-				timer.text = elTxt.toUpperCase();
-				timer.cb(elTxt);
+			// Fire if text >= options.captureLength AND text != saved text OR if override AND text >= options.captureLength
+			if ((value.length >= options.captureLength && value.toUpperCase() != timer.text)
+				|| (override && value.length >= options.captureLength))
+			{
+				timer.text = value.toUpperCase();
+				timer.cb.call(timer.el, value);
 			}
 		};
 
 		function watchElement(elem) {
-			// Must be text or textarea
-			if (elem.type.toUpperCase() == "TEXT" || elem.nodeName.toUpperCase() == "TEXTAREA") {
+			var elementType = elem.type.toUpperCase();
+			if (jQuery.inArray(elementType, options.inputTypes) >= 0) {
 
 				// Allocate timer element
 				var timer = {
-					timer : null,
-					text : jQuery(elem).val().toUpperCase(),
-					cb : options.callback,
-					el : elem,
-					wait : options.wait
+					timer: null,
+					text: jQuery(elem).val().toUpperCase(),
+					cb: options.callback,
+					el: elem,
+					wait: options.wait
 				};
 
 				// Set focus action (highlight)
@@ -57,21 +64,21 @@
 				var startWatch = function(evt) {
 					var timerWait = timer.wait;
 					var overrideBool = false;
+					var evtElementType = this.type.toUpperCase();
 
-					if (evt.keyCode == 13 && this.type.toUpperCase() == "TEXT") {
+					// If enter key is pressed and not a TEXTAREA and matched inputTypes
+					if (evt.keyCode == 13 && evtElementType != 'TEXTAREA' && jQuery.inArray(evtElementType, options.inputTypes) >= 0) {
 						timerWait = 1;
 						overrideBool = true;
 					}
 
-					var timerCallbackFx = function()
-					{
+					var timerCallbackFx = function() {
 						checkElement(timer, overrideBool)
 					}
 
-					// Clear timer
+					// Clear timer					
 					clearTimeout(timer.timer);
 					timer.timer = setTimeout(timerCallbackFx, timerWait);
-
 				};
 
 				jQuery(elem).keydown(startWatch);
@@ -79,10 +86,9 @@
 		};
 
 		// Watch Each Element
-		return this.each(function(index){
+		return this.each(function() {
 			watchElement(this);
 		});
 
 	};
-
 })(jQuery);
